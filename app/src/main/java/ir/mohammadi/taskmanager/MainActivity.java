cb3ba4f981663faf2f5501215bccd408e405245b
m_mahdi <m_galaxy74@yahoo.com>
2018-04-13 19:36:09 +0430

    design login & register

--- app/src/main/java/ir/mohammadi/taskmanager/MainActivity.java
+++ app/src/main/java/ir/mohammadi/taskmanager/MainActivity.java
@@ -1,7 +1,11 @@
 package ir.mohammadi.taskmanager;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText username = (EditText) findViewById(R.id.username);
+        final EditText password = (EditText) findViewById(R.id.password);
+
+        Button btn = (Button)findViewById(R.id.register)  ;
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(MainActivity.this,RegisterActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+
+
+
+
+
     }
 }
