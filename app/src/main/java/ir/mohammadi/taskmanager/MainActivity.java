ce4db617854c215c608dacff04318cab12512212
m_mahdi <m_galaxy74@yahoo.com>
2018-04-20 20:08:33 +0430

    logig with valley ok

--- app/src/main/java/ir/mohammadi/taskmanager/MainActivity.java
+++ app/src/main/java/ir/mohammadi/taskmanager/MainActivity.java
@@ -1,12 +1,31 @@
 package ir.mohammadi.taskmanager;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.HashMap;
+import java.util.Map;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import java.util.HashMap;
+import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,22 +33,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText username = (EditText) findViewById(R.id.username);
-        final EditText password = (EditText) findViewById(R.id.password);
+        Button button = (Button) findViewById(R.id.login);
 
-        Button btn = (Button)findViewById(R.id.register)  ;
-
-        btn.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent(MainActivity.this,RegisterActivity.class);
-                MainActivity.this.startActivity(intent);
+
+                String apiUrl = "https://api.backtory.com/auth/login";
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, apiUrl, new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String s) {
+//                        Intent intent=new Intent(MainActivity.this,TestActivity.class);
+//                        startActivity(intent);
+                        Log.i("Result", s);
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+
+
+                        Log.i("Error", volleyError.getMessage());
+                    }
+                }){
+
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("username", "mahdi74");
+                        params.put("password", "mahdi74");
+
+                        return params;
+                    }
+
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        HashMap<String, String> headers = new HashMap<String, String>();
+                        headers.put("X-Backtory-Authentication-Id", "5a9314fbe4b04e579ee1edbe");
+                        headers.put("X-Backtory-Authentication-Key", "5a9314fbe4b05bb64131ee38");
+                        return headers;
+                    }
+                };
+                stringRequest.setRetryPolicy(new DefaultRetryPolicy(2000, 2, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                queue.add(stringRequest);
+
+
+
             }
         });
-
-
-
-
 
     }
 }
